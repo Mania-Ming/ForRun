e74f3f42c8a7ac7a33a523057fbef9a6acf369ff
Mania-Ming <amaniamaepearl99@gmail.com>
2026-03-03 14:25:33 +0800

    first commit

---
+++ app/cart/page.tsx
@@ -0,0 +1,11 @@
+export default function Cart() {
+  return (
+    <div className="min-h-screen px-10 py-10">
+      <h2 className="text-3xl font-bold mb-8">Shopping Cart</h2>
+
+      <div className="bg-white p-6 rounded-xl shadow-md">
+        <p>Your cart is empty.</p>
+      </div>
+    </div>
+  );
+}
